c164049ce97813dd8274080453b4b9e3ef49f3ff
nikhil-taneja <nikhil.taneja@zoomcar.com>
2015-08-03 17:41:51 +0530

    User following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 
 has_many :microposts, dependent: :destroy
+
   attr_accessor :remember_token, :activation_token,:reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -17,6 +18,13 @@ has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
 
+has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+
+ has_many :followers, through: :passive_relationships, source: :follower
+has_many :following, through: :active_relationships, source: :followed
+
   # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -30,8 +38,13 @@ has_many :active_relationships, class_name:  "Relationship",
   end
 
 def feed
-    Micropost.where("user_id = ?", id)
-  end
+
+following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+
+end
  
  # Remembers a user in the database for use in persistent sessions.
   def remember
@@ -61,6 +74,22 @@ def feed
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+ # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 
   def downcase_email
     self.email = email.downcase
